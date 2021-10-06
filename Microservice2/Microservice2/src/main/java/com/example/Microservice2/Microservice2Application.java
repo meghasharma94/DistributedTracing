ff8bd490ffcd24c1a082f8bff61bb15eb296d723
Megha  Sharma <megha.sharmaa94@gmail.com>
2021-10-06 21:01:16 +0530

    Initial commit

---
+++ Microservice2/Microservice2/src/main/java/com/example/Microservice2/Microservice2Application.java
@@ -0,0 +1,13 @@
+package com.example.Microservice2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class Microservice2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Microservice2Application.class, args);
+	}
+
+}
